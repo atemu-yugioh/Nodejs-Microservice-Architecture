61c5a0a2525f9e27708655dbcdcf6307552f352e
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-05-04 14:15:03 +0700

    added extra infomation to understand clearly

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,5 +1,5 @@
 describe("CustomerService", () => {
-  // Which function we are testing
+  // This will explain which function we are testing!
   describe("SignIn", () => {
     test("Validate user input ", () => {});
     test("Validate response ", () => {});
