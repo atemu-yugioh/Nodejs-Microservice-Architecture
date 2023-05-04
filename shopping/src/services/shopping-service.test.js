b5bec81b6debe430e981e9b2cf6986e1df46edae
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-05-04 14:33:47 +0700

    Add CI config for other service

--- shopping/src/services/shopping-service.test.js
+++ shopping/src/services/shopping-service.test.js
@@ -1,4 +1,5 @@
 describe("ShoppingService", () => {
+  // This will explain which function we are testing!
   describe("SignIn", () => {
     test("Validate shopping input ", () => {});
     test("Validate response ", () => {});
