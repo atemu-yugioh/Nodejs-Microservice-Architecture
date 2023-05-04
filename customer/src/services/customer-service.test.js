42aa6bf1cc95f761e1441a7e9b30d9c4696b786e
Nguyễn Hữu Thiêng <55906869+atemu-yugioh@users.noreply.github.com>
2023-05-04 14:25:49 +0700

    test github flow (#11)
    * test github flow
    
    * added extra infomation to understand clearly

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,4 +1,5 @@
 describe("CustomerService", () => {
+  // This will explain which function we are testing!
   describe("SignIn", () => {
     test("Validate user input ", () => {});
     test("Validate response ", () => {});
