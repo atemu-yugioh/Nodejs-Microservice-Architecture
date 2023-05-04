3a5e3bcad6cd94763a0bf78ef8723748d8dc33a8
Nguyễn Hữu Thiêng <55906869+atemu-yugioh@users.noreply.github.com>
2023-05-04 14:37:23 +0700

    Add CI config for other service (#12)
    Add CI config for other service

--- products/src/services/product-service.test.js
+++ products/src/services/product-service.test.js
@@ -1,5 +1,6 @@
 describe("ProductService", () => {
   describe("SignIn", () => {
+    // This will explain which function we are testing!
     test("Validate product input ", () => {});
     test("Validate response ", () => {});
   });
