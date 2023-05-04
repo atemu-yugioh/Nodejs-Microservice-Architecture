8fc68f8c4260c263710296ae3f18b97acb6eb962
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-05-04 14:04:10 +0700

    test github flow

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,4 +1,5 @@
 describe("CustomerService", () => {
+  // Which function we are testing
   describe("SignIn", () => {
     test("Validate user input ", () => {});
     test("Validate response ", () => {});
